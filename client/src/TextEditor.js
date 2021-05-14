c574b6796b094d4f3fbfb7649c783f5400255a5f
gg-dev-05 <garvitgalgat@gmail.com>
2021-05-14 16:28:23 +0530

    working for single

--- client/src/TextEditor.js
+++ client/src/TextEditor.js
@@ -1,14 +1,54 @@
-import { useEffect, useRef } from "react";
+import { useEffect, useRef, useState } from "react";
 import Quill from "quill";
 import "quill/dist/quill.snow.css";
+import { io } from "socket.io-client";
 
 export default function TextEditor() {
 	const wrapperRef = useRef();
+	const [socket, setSocket] = useState();
+	const [quill, setQuill] = useState();
+
+	useEffect(() => {
+		const s = io("http://localhost:5000");
+		setSocket(s);
+
+		return () => {
+			s.disconnect();
+		};
+	}, []);
+
 	useEffect(() => {
 		const editor = document.createElement("div");
 		wrapperRef.current.innerHTML = "";
 		wrapperRef.current.append(editor);
-		new Quill(editor, { theme: "snow" });
-	});
+		const q = new Quill(editor, { theme: "snow" });
+		setQuill(q);
+	}, []);
+
+	useEffect(() => {
+		if (socket == null || quill == null) return;
+		const handler = (delta, oldDelta, source) => {
+			if (source !== "user") return;
+			socket.emit("send-changes", delta);
+		};
+		quill.on("text-change", handler);
+
+		return () => {
+			quill.off("text-change", handler);
+		};
+	}, [socket, quill]);
+
+	useEffect(() => {
+		if (socket == null || quill == null) return;
+		const handler = (delta) => {
+			quill.updateContents(delta);
+		};
+		socket.on("receive-changes", handler);
+
+		return () => {
+			socket.off("receive-changes", handler);
+		};
+	}, [socket, quill]);
+
 	return <div className="container" ref={wrapperRef}></div>;
 }
---
+++ server/server.js
@@ -0,0 +1,12 @@
+const io = require("socket.io")(5000, {
+	cors: {
+		origin: "http://localhost:3000",
+		methods: ["GET", "POST"],
+	},
+});
+
+io.on("connection", (socket) => {
+	socket.on("send-changes", (delta) => {
+		socket.broadcast.emit("receive-changes", delta);
+	});
+});

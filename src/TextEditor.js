9c0ce83c55466700ebab4e5c81ecaca23580b7ab
gg-dev-05 <garvitgalgat@gmail.com>
2021-05-14 23:58:42 +0530

    netfliy updates

--- src/TextEditor.js
+++ src/TextEditor.js
@@ -31,7 +31,7 @@ export default function TextEditor() {
 	}, [socket, quill]);
 
 	useEffect(() => {
-		const s = io("http://localhost:5000");
+		const s = io("https://api-gdocs.herokuapp.com");
 		setSocket(s);
 
 		return () => {
